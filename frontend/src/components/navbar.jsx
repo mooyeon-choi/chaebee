901bfb177f9d6eba13b519ec4a9a0830b445f037
mooyeon-choi <lon12080@gmail.com>
2022-07-28 22:43:56 +0900

    TCL-15 Router 적용 및 페이지 생성, NavBar 컴포넌트 생성

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const NavigationBar = styled.div`
+  width: 100vw;
+  height: 100px;
+`
+
+function NavBar() {
+  return ( <NavigationBar>
+  </NavigationBar> );
+}
+
+export default NavBar;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { 
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import Main from './pages/main';
+import Posts from './pages/posts';
+import Post from './pages/post';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="main" element={<Main />} />
+          <Route path="posts" element={<Posts />}>
+            <Route path=":postId" element={<Post />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
