9ba490bf3e1a60a45ee2f2a958e3260f24342587
MelProbe <melprobe@yonsei.ac.kr>
2022-07-31 16:53:04 +0900

    Spring Boot 프로젝트 추가

---
+++ backend/src/main/java/com/tcl/chaebee/ChaebeeApplication.java
@@ -0,0 +1,13 @@
+package com.tcl.chaebee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaebeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaebeeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tcl/chaebee/ChaebeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcl.chaebee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaebeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
