ee2dc599988fd65617fca7a216f56b9a8862b7dd
MelProbe <melprobe@yonsei.ac.kr>
2022-07-31 16:59:32 +0900

    테스트용 Hello Wolrd

--- backend/src/main/java/com/tcl/chaebee/ChaebeeApplication.java
+++ backend/src/main/java/com/tcl/chaebee/ChaebeeApplication.java
@@ -2,12 +2,20 @@ package com.tcl.chaebee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ChaebeeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChaebeeApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "Hello Chaebee!";
 	}
 
 }
